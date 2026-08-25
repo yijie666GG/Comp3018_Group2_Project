4f37e87f172db77555ae2419cefaab441d4086d3
22113013 <22113013@student.westernsydney.edu.au>
2026-08-25 22:20:17 +1000

    this is some of the database not complete

---
+++ mobile/app/firebase/login.js
@@ -0,0 +1,17 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "app/firebase";
+
+export async function login(email, password) {
+    try{
+        const userLoginDetails = await signInWithEmailAndPassword(
+            auth,
+            email.trim().toLowerCase(),
+            password
+        );
+
+        console.log('Login successful');
+    }
+    catch (error){
+        console.log('Login error: ', error);
+    }
+}
