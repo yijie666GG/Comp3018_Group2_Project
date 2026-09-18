457d28915d385dceff6bceb24b93b0d2de980a75
22113013 <22113013@student.westernsydney.edu.au>
2026-09-18 14:22:27 +1000

    final edit

--- mobile/app/receipt-review.tsx
+++ mobile/app/receipt-review.tsx
@@ -362,7 +362,7 @@ export default function ReceiptReview() {
             date:
               originalReceipt.date,
 
-            financialYear,
+            financialYear: financialYear,
 
             time:
               originalReceipt.time,
